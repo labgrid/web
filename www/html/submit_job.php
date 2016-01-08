b8c11cf51bc5b0aba01269926687538e23467917
Benjamin H Glick <benjamin.h.glick@gmail.com>
2016-01-08 14:42:05 -0600

    maybe attempted to check logins.  Probably did it wrong, I'll do it later.

--- www/html/submit_job.php
+++ www/html/submit_job.php
@@ -1,4 +1,7 @@
 <?php
+include 'login.php';
+include_once "../php-security/db_connect.php";
+include_once "../php-security/security_functions.php";
 
 // We use Google's supplied recaptcha code library
 require_once "../includes/recaptchalib.php";
@@ -17,10 +20,12 @@ if (isset($_POST['g-recaptcha-response'], $_POST['jobname'], $_POST['exe'], $_PO
     $jobname = $_POST['jobname'];
     $exe = $_POST['exe'];
     $file = $_POST['file'];
+    $filetype = $_POST['type']
+if(check_login($db_connection)){
+    #finish this
+   exec("python make_sub_file.py -jobname "+$jobname+" -outfile "+$jobname+".sub -executable_file "+$exe+"-filetype "+$type+"-user_args "+$file)
+}
 
-
-
-   exec("python make_sub_file.py -jobname ")
 }
 
 header("location:javascript://history.go(-1)");
